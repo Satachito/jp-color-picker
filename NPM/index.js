ca4f5d119b4e964678e6625feefaae1968498da2
Satoru Ogura <satoru.ogura@me.com>
2021-01-24 11:30:45 +0900

    INIT

---
+++ NPM/index.js
@@ -0,0 +1,258 @@
+export const
+ColorValues = {		//	https://developer.mozilla.org/ja/docs/Web/CSS/color_value
+	black					:	"#000000"
+,	silver					:	"#c0c0c0"
+,	gray					:	"#808080"
+,	white					:	"#ffffff"
+,	maroon					:	"#800000"
+,	red						:	"#ff0000"
+,	purple					:	"#800080"
+,	fuchsia					:	"#ff00ff"
+,	green					:	"#008000"
+,	lime					:	"#00ff00"
+,	olive					:	"#808000"
+,	yellow					:	"#ffff00"
+,	navy					:	"#000080"
+,	blue					:	"#0000ff"
+,	teal					:	"#008080"
+,	aqua					:	"#00ffff"
+,	orange					:	"#ffa500"
+,	aliceblue				:	"#f0f8ff"
+,	antiquewhite			:	"#faebd7"
+,	aquamarine				:	"#7fffd4"
+,	azure					:	"#f0ffff"
+,	beige					:	"#f5f5dc"
+,	bisque					:	"#ffe4c4"
+,	blanchedalmond			:	"#ffebcd"
+,	blueviolet				:	"#8a2be2"
+,	brown					:	"#a52a2a"
+,	burlywood				:	"#deb887"
+,	cadetblue				:	"#5f9ea0"
+,	chartreuse				:	"#7fff00"
+,	chocolate				:	"#d2691e"
+,	coral					:	"#ff7f50"
+,	cornflowerblue			:	"#6495ed"
+,	cornsilk				:	"#fff8dc"
+,	crimson					:	"#dc143c"
+,	cyan					:	"#00ffff"
+,	darkblue				:	"#00008b"
+,	darkcyan				:	"#008b8b"
+,	darkgoldenrod			:	"#b8860b"
+,	darkgray				:	"#a9a9a9"
+,	darkgreen				:	"#006400"
+,	darkgrey				:	"#a9a9a9"
+,	darkkhaki				:	"#bdb76b"
+,	darkmagenta				:	"#8b008b"
+,	darkolivegreen			:	"#556b2f"
+,	darkorange				:	"#ff8c00"
+,	darkorchid				:	"#9932cc"
+,	darkred					:	"#8b0000"
+,	darksalmon				:	"#e9967a"
+,	darkseagreen			:	"#8fbc8f"
+,	darkslateblue			:	"#483d8b"
+,	darkslategray			:	"#2f4f4f"
+,	darkslategrey			:	"#2f4f4f"
+,	darkturquoise			:	"#00ced1"
+,	darkviolet				:	"#9400d3"
+,	deeppink				:	"#ff1493"
+,	deepskyblue				:	"#00bfff"
+,	dimgray					:	"#696969"
+,	dimgrey					:	"#696969"
+,	dodgerblue				:	"#1e90ff"
+,	firebrick				:	"#b22222"
+,	floralwhite				:	"#fffaf0"
+,	forestgreen				:	"#228b22"
+,	gainsboro				:	"#dcdcdc"
+,	ghostwhite				:	"#f8f8ff"
+,	gold					:	"#ffd700"
+,	goldenrod				:	"#daa520"
+,	greenyellow				:	"#adff2f"
+,	grey					:	"#808080"
+,	honeydew				:	"#f0fff0"
+,	hotpink					:	"#ff69b4"
+,	indianred				:	"#cd5c5c"
+,	indigo					:	"#4b0082"
+,	ivory					:	"#fffff0"
+,	khaki					:	"#f0e68c"
+,	lavender				:	"#e6e6fa"
+,	lavenderblush			:	"#fff0f5"
+,	lawngreen				:	"#7cfc00"
+,	lemonchiffon			:	"#fffacd"
+,	lightblue				:	"#add8e6"
+,	lightcoral				:	"#f08080"
+,	lightcyan				:	"#e0ffff"
+,	lightgoldenrodyellow	:	"#fafad2"
+,	lightgray				:	"#d3d3d3"
+,	lightgreen				:	"#90ee90"
+,	lightgrey				:	"#d3d3d3"
+,	lightpink				:	"#ffb6c1"
+,	lightsalmon				:	"#ffa07a"
+,	lightseagreen			:	"#20b2aa"
+,	lightskyblue			:	"#87cefa"
+,	lightslategray			:	"#778899"
+,	lightslategrey			:	"#778899"
+,	lightsteelblue			:	"#b0c4de"
+,	lightyellow				:	"#ffffe0"
+,	limegreen				:	"#32cd32"
+,	linen					:	"#faf0e6"
+,	magenta					:	"#ff00ff"
+,	mediumaquamarine		:	"#66cdaa"
+,	mediumblue				:	"#0000cd"
+,	mediumorchid			:	"#ba55d3"
+,	mediumpurple			:	"#9370db"
+,	mediumseagreen			:	"#3cb371"
+,	mediumslateblue			:	"#7b68ee"
+,	mediumspringgreen		:	"#00fa9a"
+,	mediumturquoise			:	"#48d1cc"
+,	mediumvioletred			:	"#c71585"
+,	midnightblue			:	"#191970"
+,	mintcream				:	"#f5fffa"
+,	mistyrose				:	"#ffe4e1"
+,	moccasin				:	"#ffe4b5"
+,	navajowhite				:	"#ffdead"
+,	oldlace					:	"#fdf5e6"
+,	olivedrab				:	"#6b8e23"
+,	orangered				:	"#ff4500"
+,	orchid					:	"#da70d6"
+,	palegoldenrod			:	"#eee8aa"
+,	palegreen				:	"#98fb98"
+,	paleturquoise			:	"#afeeee"
+,	palevioletred			:	"#db7093"
+,	papayawhip				:	"#ffefd5"
+,	peachpuff				:	"#ffdab9"
+,	peru					:	"#cd853f"
+,	pink					:	"#ffc0cb"
+,	plum					:	"#dda0dd"
+,	powderblue				:	"#b0e0e6"
+,	rosybrown				:	"#bc8f8f"
+,	royalblue				:	"#4169e1"
+,	saddlebrown				:	"#8b4513"
+,	salmon					:	"#fa8072"
+,	sandybrown				:	"#f4a460"
+,	seagreen				:	"#2e8b57"
+,	seashell				:	"#fff5ee"
+,	sienna					:	"#a0522d"
+,	skyblue					:	"#87ceeb"
+,	slateblue				:	"#6a5acd"
+,	slategray				:	"#708090"
+,	slategrey				:	"#708090"
+,	snow					:	"#fffafa"
+,	springgreen				:	"#00ff7f"
+,	steelblue				:	"#4682b4"
+,	tan						:	"#d2b48c"
+,	thistle					:	"#d8bfd8"
+,	tomato					:	"#ff6347"
+,	turquoise				:	"#40e0d0"
+,	violet					:	"#ee82ee"
+,	wheat					:	"#f5deb3"
+,	whitesmoke				:	"#f5f5f5"
+,	yellowgreen				:	"#9acd32"
+,	rebeccapurple			:	"#663399"
+}
+
+export const
+Y_601	= ( r, g, b ) => 0.299  * r + 0.587  * g + 0.114  * b
+export const
+Y_709	= ( r, g, b ) => 0.2126 * r + 0.7152 * g + 0.0722 * b
+
+export const
+RGBA_HTMLColor = $ => {
+	const
+	RGBA_HexColor = ( $, length ) => [
+		parseInt( $.substr( 1 + length * 0, length ), 16 ) / ( 2 ** ( length * 4 ) - 1 )
+	,	parseInt( $.substr( 1 + length * 1, length ), 16 ) / ( 2 ** ( length * 4 ) - 1 )
+	,	parseInt( $.substr( 1 + length * 2, length ), 16 ) / ( 2 ** ( length * 4 ) - 1 )
+	,	parseInt( $.substr( 1 + length * 3, length ), 16 ) / ( 2 ** ( length * 4 ) - 1 )
+	]
+	if ( $.charAt( 0 ) === '#' ) {
+		switch ( $.length ) {
+		case 4:	return RGBA_HexColor( $ + 'f', 1 )
+		case 5:	return RGBA_HexColor( $, 1 )
+		case 7:	return RGBA_HexColor( $ + 'ff', 2 )
+		case 9:	return RGBA_HexColor( $, 2 )
+		}
+	} else {
+		const _ = ColorValues[ $ ]
+		if ( _ ) return RGBA_HexColor( _ + 'ff', 2 )
+	}
+	throw 'RGBA_HTMLColor'
+}
+
+const
+HexColor7 = $ => {
+	if ( $.charAt( 0 ) === '#' ) {
+		switch ( $.length ) {
+		case 4:
+		case 5:	return $.charAt( 0 ) + $.charAt( 1 ) + $.charAt( 1 ) + $.charAt( 2 ) + $.charAt( 2 ) + $.charAt( 3 ) + $.charAt( 3 )
+		case 7:	return $
+		case 9:	return $.substr( 0, 7 )
+		}
+	} else {
+		const _ = ColorValues[ $ ]
+		if ( _ ) return _
+	}
+	throw 'HexColor7'
+}
+
+class
+JPColorPicker extends HTMLElement {
+
+	constructor( color ) {
+		super()
+
+		color || ( color = this.getAttribute( 'color' ) ?? 'black' )
+
+		this.style.display = 'inline-block'
+//		this.style.border = 'solid 1px black'
+
+		const
+		sr = this.attachShadow( { mode: 'open' } )
+
+		const
+		datalist = sr.appendChild( document.createElement( 'datalist' ) )
+		datalist.id = 'Colors'
+		datalist.append(
+			...Object.keys( ColorValues ).sort().map(
+				$ => {
+					const _ = document.createElement( 'option' )
+					_.value = $
+					return _
+				}
+			)
+		)
+
+		this.list = sr.appendChild( document.createElement( 'input' ) )
+		this.list.setAttribute( 'list', 'Colors' )
+		this.list.value = color
+		this.list.style.borderStyle = 'none'
+		this.list.style.margin = '0 4px'
+		this.list.style.transform = 'translateY( -2px )'
+
+		this.palette = sr.appendChild( document.createElement( 'input' ) )
+		this.palette.type = 'color'
+		this.palette.style.borderStyle = 'none'
+
+		this.list.onchange = () => this.SyncPalette( this.list.value ) 
+		this.palette.oninput = $ => (
+			this.list.value = this.palette.value
+		,	this.palette.style.opacity = 1
+		)
+	}
+
+	SyncPalette( $ ) {
+		this.palette.value = HexColor7( $ )
+		const [ R, G, B, A ] = RGBA_HTMLColor( $ )
+		this.palette.style.opacity = A
+	}
+
+	static get
+	observedAttributes() { return [ 'color' ] }
+	attributeChangedCallback( name, o, n ) {
+		this.list.value = n
+		this.SyncPalette( n )
+	}
+}
+
+customElements.define( 'jp-color-picker', JPColorPicker )
+
+export default JPColorPicker
